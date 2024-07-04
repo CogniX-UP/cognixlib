d3bcdb3b543e13f48c7e964f674d79a7c6e3e121
Georgios Papadoulis <gg.papadoulis@gmail.com>
2024-07-04 09:08:59 +0300

    Filters [WIP]

--- cognixlib/scripting/processing/filters.py
+++ cognixlib/scripting/processing/filters.py
@@ -5,44 +5,36 @@ This module's approach is object-oriented and not function oriented
 """
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from collections.abc import Sequence
 from ..data import Signal, StreamSignal, StreamSignalInfo
 from mne.filter import (
     create_filter,
     _overlap_add_filter,
     _iir_filter,
 )
+from scipy.signal import oaconvolve, convolve
+from FIRconv import FIRfilter
 from dataclasses import dataclass
 from enum import StrEnum
 
 import numpy as np
 
-class Phase(StrEnum):
-    ZERO="zero"
-    ZERO_DOUBLE="zero-double"
-    MINIMUM="minimum"
-    MINIMUM_HALF="minimum-half"
-
-class FilterWindow(StrEnum):
-    """Defines standard window types"""
-    HAMMING="hamming"
-    HANN="hann"
-    BLACKMAN="blackman"
+class FilterType(StrEnum):
+    LOWPASS='lowpass'
+    HIGHPASS='highpass'
+    BANDPASS='bandpass'
+    BANDSTOP='bandstop'
     
-@dataclass
-class FilterParams:
-    """
-    Specifies standard filter parameters common to many filter
-    types from the sci-py library.
-    """
-    low_freq: float = None
-    high_freq: float = None
-    l_trans_bandwidth: float = "auto"
-    h_trans_bandwidth: float = "auto"
-    filter_length: str | int = "auto"
+class FilterApplier(ABC):
+    """The basic definition of an object that applies a filter"""
     
-class Filter(ABC):
-    """The basic definition of a filter"""
+    class Phase(StrEnum):
+        PASS='pass'
+        """Applies the filter once. Known also as the forward method."""
+        ZERO='zero'
+        """
+        Applies the filter so that it removes phase distortion. Known also
+        as forward-backwards method.
+        """
         
     @classmethod
     @abstractmethod
@@ -50,123 +42,199 @@ class Filter(ABC):
         pass
     
     @abstractmethod
-    def filter(self, signal: Signal, copy=True) -> Signal:
+    def filter(
+        self, 
+        signal: Signal | np.ndarray
+    ) -> Signal | np.ndarray | None:
+        """
+        Filters an incoming signal or numpy array.
+        
+        An implementation of this method should consider that many filters
+        can be applied by the same object, hence the possibility of a 
+        :code:`Sequence` return type.
+        
+        Furthermore, the incoming signal type should be identical to the input
+        type.
+        """
         pass
 
-class MNEFilter(Filter):
-    
-    @classmethod
-    def ensure_correct_type(cls, signal: Signal) -> Signal:
-        if signal.data.dtype != np.float64:
-            signal = signal.copy(False)
-            signal.data = signal.data.astype(np.float64)
-        return signal 
-    
-class FIRFilter(MNEFilter):
+class FIRApplier(FilterApplier):
     """
-    Creates an FIR filter that handles the typical cases of lowpass, highpass, bandpass and bandstop.
-    
-    The :code:`filter` method has not been implemented.
+    FIR Filter Applier 
     """
-
-    def __init__(
-        self, 
-        sfreq: float | StreamSignalInfo,
-        params: FilterParams,
-        phase: Phase = Phase.ZERO,
-        wnd: FilterWindow = FilterWindow.HAMMING,
-        data_valid: Signal = None
-    ):
-        self._params = params
-        self._phase = phase
-        self.sfreq = sfreq
-        if isinstance(sfreq, StreamSignalInfo):
-            self.sfreq = sfreq.nominal_srate
-        
-        if data_valid:
-            data_valid = self.ensure_correct_type(data_valid) 
-            data_check = data_valid.data.T if data_valid else None
-        else:
-            data_check = None
-        
-        self._fir = create_filter(
-            data=data_check,
-            sfreq=self.sfreq,
-            l_freq=params.low_freq,
-            h_freq=params.high_freq,
-            filter_length=params.filter_length,
-            method="fir",
-            phase=phase,
-            fir_window=wnd,
-            fir_design="firwin"
-        )
-
-class FIROfflineFilter(FIRFilter):
-    
-    class Mode(StrEnum):
-        MNE = 'mne'
-        SCIPY = 'scipy'
-        
-        
-    def __init__(
-        self, 
-        sfreq: float | StreamSignalInfo, 
-        params: FilterParams, 
-        phase: Phase = Phase.ZERO, 
-        wnd: FilterWindow = FilterWindow.HAMMING, 
-        data_valid: Signal = None
-    ):
-        super().__init__(sfreq, params, phase, wnd, data_valid)
-        
-    def filter(self, signal: Signal, copy=True) -> Signal:
-        _overlap_add_filter()
-        return super().filter(signal, copy)
-
-# TODO Refactor
-class IIRFilter(MNEFilter):
-    """An implementation of an IIR Filter"""
     
     def __init__(
         self,
-        sfreq: float | StreamSignalInfo,
-        params: FilterParams,
-        phase: Phase = Phase.ZERO,
-        data_valid: Signal=None
+        h: np.ndarray,
+        channels=1
     ):
-        self._params = params
+        self._h = h
+        self._h_apply = h
+        if self._h.ndim == 1 and channels > 1:
+            self._h_apply = np.tile(h, (channels, 1)).T
+        
+        self._changed = False
+        super().__init__()
+    
+    @property
+    def h(self) -> np.ndarray:
+        return self._h
+    
+    @h.setter
+    def h(self, value: np.ndarray):
+        self._h = value
+        self._h_apply = value
+        self._changed = True
+    
+    def _reconstruct_h(self, data: np.ndarray):
+        if (
+            not self._changed and
+            self._h_apply.shape  == data.shape
+        ):
+            return
+        
+        self._changed = False
+        self._h_apply = self._h
+        channels = data.shape[1]
+        if self._h.ndim == 1:
+            self._h_apply = np.tile(self._h, (channels, 1)).T
+
+class FIROfflineApplier(FIRApplier):
+    """
+    Filters the whole signal using an FIR filter. Should always return the same size as the signal.
+    
+    If the FIR designed has a linear-phase, we can shift the signal instead of forward-backward
+    filtering it to remove delays. This is close enough to zero-phase filtering. Otherwise, we 
+    could simply forward-backward filter it, which is more accurate but requires more time and
+    will have a slight change in amplitude.
+    """
+    
+    class Phase(StrEnum):
+        PASS='pass'
+        """Applies the filter once. Known also as the forward method."""
+        ZERO='zero'
+        """
+        Applies the filter so that it removes phase distortion. Known also
+        as forward-backwards method.
+        """
+        ZERO_SHIFT='zero-shift'
+        """
+        Applies the filter once and accounts for phase distortion by shifting
+        it to the left. This is faster than the :code:`ZERO` method, but only
+        works when we have a linear phase FIR filter. 
+        """
+        
+    class Method(StrEnum):
+        """The method with which to apply the convolution in the :code:`FIROfflineApplier` filter."""
+        # AUTO='auto'
+        DIRECT='direct'
+        FFT='fft'
+        OVERLAP_ADD='overlap-add'
+        
+    def __init__(
+        self, 
+        h: np.ndarray, 
+        channels=1,
+        phase: Phase = Phase.ZERO_SHIFT,
+        method: Method = Method.OVERLAP_ADD,
+        remove_delay=True,
+    ):
+        super().__init__(h, channels)
         self._phase = phase
-        self.sfreq = sfreq
-        if isinstance(sfreq, StreamSignalInfo):
-            self.sfreq = sfreq.nominal_srate
+        self._method = method
+        self._remove_delay = remove_delay
+    
+    def filter(
+        self, 
+        signal: Signal | np.ndarray,
+    ) -> Signal | np.ndarray | None:
+
+        is_signal = isinstance(signal, Signal)
+        data = signal.data if is_signal else signal
+        samples, channels = data.shape[0]
+        self._reconstruct_h(data)
         
-        if data_valid:
-            data_valid = self.ensure_correct_type(data_valid) 
-            data_check = data_valid.data.T
+        foa = FIROfflineApplier
+        phase_mode = 'full' if self._phase == foa.Phase.PASS else 'same'
+        
+        # Using the notation [0:samples] means that we're getting the
+        # correct result for either Phase.ZERO or Phase.PASS
+        
+        if self._method == foa.Method.OVERLAP_ADD:
+            conv_res = oaconvolve(data, self._h_apply, mode=phase_mode, axes=0)[0:samples]
+            if self._phase == foa.Phase.ZERO:
+                h_r = self._h_apply[::-1]
+                conv_res = oaconvolve(conv_res, h_r, mode=phase_mode, axes=0)
         else:
-            data_check = None
+            conv_res = np.zeros((samples, channels))
+            # method == methdo for convolve in DIRECT or FFT
+            for i in range(channels):
+                conv_res[:, i] = convolve(
+                    data[:,i], 
+                    self._h_apply[:, i],
+                    mode=phase_mode,
+                    method=self._method
+                )[0:samples]
+            
+            if self._phase == foa.Phase.ZERO:
+                h_r = self._h_apply[::-1]
+                for i in range(channels):
+                    conv_res[:, i] = convolve(
+                        conv_res[:,i],
+                        h_r,
+                        mode=phase_mode,
+                        method=self._method
+                    )
         
-        self._irr = create_filter(
-            data=data_check,
-            l_freq=params.low_freq,
-            h_freq=params.high_freq,
-            phase=phase,
-            method="iir",
-            filter_length=params.filter_length,
-        )
+        if not is_signal:
+            return conv_res
         
-    def filter(self, signal: Signal, copy=True):
-        result = signal.copy(False)
-        result = self.ensure_correct_type(result)
+        sig_res = signal.copy(False)
+        sig_res.data = conv_res
+        return sig_res
         
-        filt_data = _iir_filter(
-            result.data.T,
-            self._irr,
-            None,
-            -1,
-            copy,
-            self._phase,
-        )
-        result.data = filt_data.T
-        return result.data
+class FIROnlineApplier(FIRApplier):
+    """
+    Designed to filter an incoming signal using an FIR (Finite Impulse Response). As this
+    is online filtering, it needs to be causal, hence zero-phase is not achievable.
+    """
+    
+    class Method(StrEnum):
+        OVERLAP_ADD = 'overlap-add'
+        OVERLAP_SAVE = 'overlap-save'
+        UPOLS = 'upols'
         
+    def __init__(
+        self, 
+        h: np.ndarray, 
+        channels=1,
+        method=Method.OVERLAP_SAVE,
+        block_size=512
+    ):
+        super().__init__(h, channels)
+        
+        self._method = method
+        self._block_size = block_size
+        self._applier = FIRfilter(method, block_size, self._h_apply)
+        
+        # initial buffer
+        self._buffer = np.zeros(2*block_size, channels)
+        self._curr_idx = 0
+    
+    def filter(
+        self, 
+        signal: Signal | np.ndarray
+    ) -> Signal | np.ndarray | None:
+        
+        return super().filter(signal)
+        
+        
+        
+
+
+# TODO implement this
+class FIRApplierMNE(FIRApplier):
+    pass
+    
+
         
--- tests/filter_test.py
+++ tests/filter_test.py
@@ -1,7 +1,8 @@
 """
 Some good-to-know facts.
 
-1)  MNE's filter function is really slow, for some reason. At least for FIR filters.
+1)  MNE's filter function is really slow, for some reason. At least for FIR filters. It's only fast for larger
+    signals.
 2)  scipy's oaconvole is quite faster than MNE's corresponding overlap-add method.
     Furthermore, there is a mode for removing phase lag/delay.
 3)  In a real-time BCI setting, there will always be a phase delay. In essence, we
@@ -11,6 +12,22 @@ Some good-to-know facts.
     For BCIs, we should always aim for minimum phase-delay, since it is not possible
     to remove the delay in real-time (to remove it we'd need future values)
 4)  We really need to parallelize stuff and not between processes.
+
+lfilter == np.convolve == real time
+
+In realtime, applying lfilter and np.convolve and real time FIRfilter all yield the
+same result.
+
+For attempting to correct phase delay, filtfilt, oaconvolve and mne all yield different
+results.
+
+For FIR filters, the forward-backwards and time shifting are identical bar the change
+in amplitude. If we don't have FIR filters, then it's better to do forward-backward
+for zero-phase filtering.
+
+Finally, we have edge and transient effects. I think these are automatically handled when
+using overlap-add in the case of FIR filters. Either way, in offline situations, this shouldn't
+matter since after the transient effects the filtering is identical.
 """
 
 if __name__ != '__main__':
@@ -18,7 +35,7 @@ if __name__ != '__main__':
     
 import mne
 from FIRconv import FIRfilter
-from scipy.signal import oaconvolve
+from scipy.signal import oaconvolve, firwin, minimum_phase, convolve, filtfilt, lfilter
 from scipy.fft import fft, fftfreq
 from mne.filter import create_filter, _overlap_add_filter
 import numpy as np
@@ -30,91 +47,111 @@ from concurrent.futures import ThreadPoolExecutor
 from threading import Thread
 
 fs = 2048
-t = int(fs * 2)
-channels = 16
+# t = int(fs*10800) # 3 hours
+t = int(fs*1) # 1 second
+channels = 8
 channel_in = np.longdouble(35 * np.random.rand(t, channels))
-print(channel_in.shape)
 
+#%%
+h_t = firwin(845, [8, 30], pass_zero='bandpass', fs=fs)
+# k = minimum_phase(h_t)
+#%%
 h = create_filter(
     data=None, 
     sfreq=fs, 
-    l_freq=20, 
-    h_freq=24,
-    l_trans_bandwidth=2,
-    h_trans_bandwidth=2,
-    phase='linear'
+    l_freq=8, 
+    h_freq=30,
+    l_trans_bandwidth=8,
+    h_trans_bandwidth=8,
+    phase='zero-double'
 )
+# h = h_t
 
-np.apply_along_axis
 t0 = time.perf_counter()
 # this is much faster due to subprocess copying data
 with parallel_backend('threading'):
-    mne_res = _overlap_add_filter(channel_in.T, h, phase="linear",n_jobs=-1)
+    mne_res = _overlap_add_filter(channel_in.T, h, phase="zero",n_jobs=-1)
 t1 = time.perf_counter()
 print(f"MNE RES: {t1-t0}")
 
 h_m = np.tile(h, (channels, 1)).T
 t0 = time.perf_counter()
-sci_res = oaconvolve(channel_in, h_m, 'same', 0)
-# sci_res = np.zeros((t, channels))
-# for i in range(channels):
-#    sci_res[:,i] = oaconvolve(channel_in[:,i], h, mode="same")
+filt_filt = filtfilt(h, 1, channel_in, axis=0, padtype='even', padlen=50)
 t1 = time.perf_counter()
-print(f"SCI RES: {t1-t0}")
+print(f"FILT FILT RES: {t1-t0}")
 
 t0 = time.perf_counter()
-t_l: list[Thread] = []
-for i in range(channels):
-    def do():
-        sci_res[:, i] = oaconvolve(channel_in[:, i], h, mode='same')
-    thr = Thread(target=do)
-    thr.start()
-    t_l.append(thr)
-
-for thr in t_l:
-    thr.join()
+pad = 50
+# ch = np.pad(channel_in, [(pad, pad), (0, 0)], mode='reflect') # how to pad like filtfilt
+sci_res = oaconvolve(channel_in, h_m, 'same', 0)
+h_r = h_m[::-1]
+sci_res = oaconvolve(sci_res, h_r, 'same', 0)[0:t]
 t1 = time.perf_counter()
-print(f"SCI RES Threads: {t1-t0}")
+print(f"SCIPY RES: {t1-t0}")
     
-step = 512
+step = 256
 real_res = np.zeros((t, channels))
-fir = FIRfilter(method='upols', blockSize=step, h=h_m, normalize=False)
+fir = FIRfilter(method='ols', blockSize=step, h=h_m, normalize=False)
 frame_start = 0
 frame_end = step
+
 t0 = time.perf_counter()
 index = 0
-
 while frame_end <= channel_in.shape[0]:
     index += 1
     res = fir.process(channel_in[frame_start:frame_end])
     real_res[frame_start:frame_end] = res
     frame_start = frame_end
     frame_end += step
-        
     
 t1 = time.perf_counter()
+
 print(f"REAL RES: {(t1-t0)/index}")
 t0 = time.perf_counter()
 np_res = np.zeros((t, channels))
 for i in range(channels):
-    np_res[:, i] = np.convolve(channel_in[:,i], h)[0:t]
+    np_res[:, i] = convolve(channel_in[:,i], h)[0:t]
 t1 = time.perf_counter()
 print(f"NP RES: {t1-t0}")
-print(mean_squared_error(real_res[:,0], np_res[:,0]))
+
+t0 = time.perf_counter()
+l_filt = lfilter(h, 1, channel_in, axis=0)
+t1 = time.perf_counter()
+print(f"Lfilter RES: {t1-t0}")
+
+fig, axs = plt.subplots(2, 1)
+top = axs[0]
+
+top.plot(filt_filt[:, 0], label='Filt Filt Signal')
+top.plot(sci_res[:, 0], label='SciPy Filtered Signal')
+top.plot(mne_res.T[:,0], label='MNE Filtered Signal', linestyle='dashed')
+#plt.plot(np.abs(filt_filt[:, 3]-sci_res[:,3]), label='Diff')
+top.legend()
+top.set_title('Comparison Zero Phase')
+
+bot = axs[1]
+bot.plot(real_res[:,0], label='Real Time Filtered')
+bot.plot(np_res[:,0], label="NP Convole")
+bot.plot(l_filt[:, 0], label='Lfilter')
+bot.legend()
+bot.set_title('Comparison of Real Time')
 
 plt.figure()
-plt.plot(sci_res[:,0], label='SciPy Filtered Signal')
-plt.plot(mne_res.T[:,0], label='MNE Filtered Signal', linestyle='dashed')
-plt.plot(real_res[:,0], label='Real Time Filtered')
-plt.plot(np_res[:,0], label="NP Convole")
+ind = slice(0, 100) 
+
+fft_res = fft(h)[ind]
+fft_freq = fftfreq(len(h), d=1/fs)[ind]
+plt.plot(fft_freq, np.abs(fft_res), label='Frequence Response MNE')
+
+fft_zero = fft(np.convolve(h, h[::-1], mode='same'))[ind]
+fft_zero_freq = fftfreq(len(h), d=1/fs)[ind]
+plt.plot(fft_zero_freq, np.abs(fft_zero), label='Zero Frequence Response MNE')
+
+fft_res_ht = fft(h_t)[ind]
+fft_freq_ht = fftfreq(len(h_t), d=1/fs)[ind]
+plt.plot(fft_freq_ht, np.abs(fft_res_ht), label='Freq Response SCIPY')
+
 plt.legend()
-plt.title('Comparison of Filtered Signals')
-
-plt.figure()
-fft_res = fft(h)
-fft_freq = fftfreq(len(h), d=1/fs)
-ind = slice(0, 80)
-plt.plot(fft_freq[ind], np.abs(fft_res)[ind], label='Frequence Response')
 plt.title('Filter FFT')
 
-plt.show()
+# plt.show()
